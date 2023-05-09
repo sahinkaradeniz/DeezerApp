1154525476b74845f6af663253fbd52e9cebf279
Sahin Karadeniz <sahinkaradeniiz@gmail.com>
2023-05-09 18:43:58 +0300

    genre dto added

---
+++ data/src/main/java/com/example/data/dto/genre/MusicGenreResponse.kt
@@ -0,0 +1,7 @@
+package com.example.data.dto.genre
+
+import com.google.gson.annotations.SerializedName
+
+data class MusicGenreResponse (
+    @SerializedName("data" ) var data : ArrayList<MusicGenreData>
+        )
