760116015718a4354decd5867436c217fc2fed26
Sahin Karadeniz <sahinkaradeniiz@gmail.com>
2023-05-09 19:13:02 +0300

    getGenreArtistsWithGenreId api call added

--- data/src/main/java/com/example/data/api/DeezerApi.kt
+++ data/src/main/java/com/example/data/api/DeezerApi.kt
@@ -1,9 +1,16 @@
 package com.example.data.api
 
+import com.example.data.dto.artists.MusicArtistsResponse
 import com.example.data.dto.genre.MusicGenreResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface DeezerApi {
     @GET("genre")
-    suspend fun getGenresOfMusic():MusicGenreResponse
+    suspend fun getGenresOfMusic(): MusicGenreResponse
+
+    @GET("genre/{genre_id}/artists")
+    suspend fun getGenreArtistsWithGenreId(
+        @Path("genre_id") genre_id: Int,
+    ): MusicArtistsResponse
 }
