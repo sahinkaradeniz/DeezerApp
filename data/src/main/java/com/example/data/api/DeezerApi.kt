58856376d0635e9cfc183f27c63221fc48cdb31c
Sahin Karadeniz <sahinkaradeniiz@gmail.com>
2023-05-09 18:55:40 +0300

    getGenresOfMusic api call added

--- data/src/main/java/com/example/data/api/DeezerApi.kt
+++ data/src/main/java/com/example/data/api/DeezerApi.kt
@@ -1,4 +1,9 @@
 package com.example.data.api
 
+import com.example.data.dto.genre.MusicGenreResponse
+import retrofit2.http.GET
+
 interface DeezerApi {
+    @GET("genre")
+    suspend fun getGenresOfMusic():MusicGenreResponse
 }
