88bea9f504cc60fa423259a3a9f5a1550336b797
Sahin Karadeniz <sahinkaradeniiz@gmail.com>
2023-05-10 18:23:37 +0300

    Add methods to FavoritesDao

--- data/src/main/java/com/example/data/datastore/room/FavoritesDao.kt
+++ data/src/main/java/com/example/data/datastore/room/FavoritesDao.kt
@@ -1,7 +1,23 @@
 package com.example.data.datastore.room
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.data.dto.favorite.FavoritesDbModel
 
 @Dao
 interface FavoritesDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addSongToFavorites(song: FavoritesDbModel)
+    @Delete
+    suspend fun deleteSongToFavorites(song: FavoritesDbModel)
+
+    @Query("select * from favorites_table")
+    suspend fun getAllSongs():List<FavoritesDbModel>
+
+    @Query("select * from favorites_table where :id")
+    suspend fun getFavoriteSongWithId(id:Int):FavoritesDbModel
+    
 }
