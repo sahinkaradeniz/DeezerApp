6d36949bcb8e3465e9e60a75a4e0cf02adbbc909
Sahin Karadeniz <sahinkaradeniiz@gmail.com>
2023-05-10 18:14:45 +0300

    create favorites database

---
+++ data/src/main/java/com/example/data/di/room/FavoritesDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.data.di.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.data.datastore.room.FavoritesDao
+import com.example.data.dto.favorite.FavoritesDbModel
+
+@Database(entities = [FavoritesDbModel::class], version = 1, exportSchema = false)
+abstract class FavoritesDatabase:RoomDatabase() {
+    abstract fun favoritesDao(): FavoritesDao
+}
