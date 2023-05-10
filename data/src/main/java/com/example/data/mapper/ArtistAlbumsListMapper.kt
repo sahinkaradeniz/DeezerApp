82ed6b63fadbfb7e3a0dd6beed8001eb35218198
Sahin Karadeniz <sahinkaradeniiz@gmail.com>
2023-05-10 21:01:18 +0300

    artist albums list mapper added

---
+++ data/src/main/java/com/example/data/mapper/ArtistAlbumsListMapper.kt
@@ -0,0 +1,19 @@
+package com.example.data.mapper
+
+import com.example.common.mapper.DeezerListMapper
+import com.example.data.dto.artist_albums.ArtistAlbumsData
+import com.example.domain.entity.ArtistAlbumsEntity
+
+class ArtistAlbumsListMapper:DeezerListMapper<ArtistAlbumsData,ArtistAlbumsEntity> {
+    override fun map(input: List<ArtistAlbumsData>): List<ArtistAlbumsEntity> {
+        return input.map {
+            ArtistAlbumsEntity(
+                id = it.id,
+                title = it.title,
+                tracklist = it.tracklist,
+                type = it.type,
+                picture = it.coverMedium
+            )
+        }
+    }
+}
