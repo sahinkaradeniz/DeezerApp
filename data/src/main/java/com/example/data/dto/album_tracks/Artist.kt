12421f517c1c4a396e13dd1e94d003832ab97e79
Sahin Karadeniz <sahinkaradeniiz@gmail.com>
2023-05-10 22:39:10 +0300

    album tracks list mapper added

--- data/src/main/java/com/example/data/dto/album_tracks/Artist.kt
+++ data/src/main/java/com/example/data/dto/album_tracks/Artist.kt
@@ -3,9 +3,9 @@ package com.example.data.dto.album_tracks
 import com.google.gson.annotations.SerializedName
 
 data class Artist (
-    @SerializedName("id"        ) var id        : Int?    = null,
-    @SerializedName("name"      ) var name      : String? = null,
-    @SerializedName("tracklist" ) var tracklist : String? = null,
-    @SerializedName("type"      ) var type      : String? = null
+    @SerializedName("id"        ) var id        : Int,
+    @SerializedName("name"      ) var name      : String,
+    @SerializedName("tracklist" ) var tracklist : String,
+    @SerializedName("type"      ) var type      : String
 
 )
---
+++ data/src/main/java/com/example/data/mapper/AlbumTracksListMapper.kt
@@ -0,0 +1,20 @@
+package com.example.data.mapper
+
+import com.example.common.mapper.DeezerListMapper
+import com.example.data.dto.album_tracks.AlbumTracksData
+import com.example.domain.entity.AlbumTracksEntity
+
+class AlbumTracksListMapper:DeezerListMapper<AlbumTracksData,AlbumTracksEntity> {
+    override fun map(input: List<AlbumTracksData>): List<AlbumTracksEntity> {
+        return input.map {
+            AlbumTracksEntity(
+                id = it.id,
+                duration = it.duration,
+                artist = it.artist.name ,
+                preview = it.preview,
+                type = it.type,
+                title = it.title
+            )
+        }
+    }
+}
