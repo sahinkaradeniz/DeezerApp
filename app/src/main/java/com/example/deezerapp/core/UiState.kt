25bf84b656bc0c942c1c699c682caa7bef926987
Sahin Karadeniz <sahinkaradeniiz@gmail.com>
2023-05-11 16:52:53 +0300

    add methods to GenresViewModel

--- app/src/main/java/com/example/deezerapp/core/UiState.kt
+++ app/src/main/java/com/example/deezerapp/core/UiState.kt
@@ -3,5 +3,5 @@ package com.example.deezerapp.core
 sealed class UiState<out T> {
     object Loading : UiState<Nothing>()
     data class Success<T>(val data: T?) : UiState<T>()
-    data class Error(val message: Int) : UiState<Nothing>()
+    data class Error(val message: String) : UiState<Nothing>()
 }
