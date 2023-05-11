e9798f1848755cf2ebada1ddf04bc3a3a1d4d626
Sahin Karadeniz <sahinkaradeniiz@gmail.com>
2023-05-11 22:38:58 +0300

    update artist fragment

--- app/src/main/java/com/example/deezerapp/ui/artist/ArtistFragment.kt
+++ app/src/main/java/com/example/deezerapp/ui/artist/ArtistFragment.kt
@@ -6,10 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.common.extension.downloadFromUrl
 import com.example.deezerapp.R
 import com.example.deezerapp.core.BaseFragment
+import com.example.deezerapp.core.UiState
 import com.example.deezerapp.databinding.FragmentArtistBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,9 +22,38 @@ class ArtistFragment : BaseFragment<FragmentArtistBinding>(FragmentArtistBinding
 
     private val viewModel: ArtistViewModel by viewModels()
     private val args :ArtistFragmentArgs by navArgs()
+    private val adapter by lazy { AlbumsAdapter(::clickAlbumItem) }
+
+    private fun clickAlbumItem(albumId: Int) {
+        Toast.makeText(requireContext(), albumId.toString(), Toast.LENGTH_SHORT).show()
+    }
 
     override fun onCreateFinished() {
+        binding.albumsRcv.adapter=adapter
+        binding.albumsRcv.layoutManager=LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,true)
+        observeLiveData()
         val artistId=args.artistId
+        viewModel.fetchArtistData(artistId)
+    }
+    private fun observeLiveData(){
+        viewModel.artistUiState.observe(viewLifecycleOwner){
+            when(it){
+                is UiState.Loading ->{
+                    showProgress()
+                }
+                is UiState.Success->{
+                    it.data?.let { artistData ->
+                        binding.artistPicture.downloadFromUrl(artistData.picture)
+                        binding.artistName.text=artistData.name
+                        adapter.updateItems(artistData.albums)
+                    }
+                    hideProgress()
+                }
+                is UiState.Error->{
+                    Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
 }
