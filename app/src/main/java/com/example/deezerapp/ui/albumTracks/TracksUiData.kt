b512f042a87b363ab93c14ff72cabde7f9a8807f
Sahin Karadeniz <sahinkaradeniiz@gmail.com>
2023-05-12 14:19:59 +0300

    refactor

--- app/src/main/java/com/example/deezerapp/ui/albumTracks/TracksUiData.kt
+++ app/src/main/java/com/example/deezerapp/ui/albumTracks/TracksUiData.kt
@@ -1,9 +1,13 @@
 package com.example.deezerapp.ui.albumTracks
 
+import com.example.domain.entity.FavoritesEntity
+
 data class TracksUiData(
     var id: Int,
     var title: String,
     var duration: Int,
     var preview: String,
     var artist: String,
+    var picture:String="",
+    var isFavorite:Boolean=false
 )
