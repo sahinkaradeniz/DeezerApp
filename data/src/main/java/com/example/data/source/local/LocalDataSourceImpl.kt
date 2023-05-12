de165d036cb3c37db03b5a9002df287a7de454a5
Sahin Karadeniz <sahinkaradeniiz@gmail.com>
2023-05-12 23:21:31 +0300

    add javadoc comments to local-remote data source

--- data/src/main/java/com/example/data/source/local/LocalDataSourceImpl.kt
+++ data/src/main/java/com/example/data/source/local/LocalDataSourceImpl.kt
@@ -6,27 +6,24 @@ import com.example.data.datastore.room.FavoritesDao
 import com.example.data.dto.favorite.FavoritesDbModel
 import javax.inject.Inject
 
+/**
+ * LocalDataSourceImpl is the implementation of the local data source.
+ * It performs operations related to favorite songs.
+ */
 class LocalDataSourceImpl @Inject constructor(
-   private val favoritesDao: FavoritesDao
-):LocalDataSource {
+    private val favoritesDao: FavoritesDao,
+) : LocalDataSource {
+    /**
+     * Adds a song to favorites.
+     *
+     * @param favoritesDbModel The FavoritesDbModel to be added.
+     * @return A ResponseResult containing the added FavoritesDbModel.
+     */
     override suspend fun addSongToFavorites(favoritesDbModel: FavoritesDbModel): ResponseResult<FavoritesDbModel> {
-       return try {
-             favoritesDao.addSongToFavorites(favoritesDbModel)
-            ResponseResult.Success(favoritesDbModel)
-       }catch (e:Exception){
-           ResponseResult.Error.IOException(
-               ResultError(
-                   errorMessage = e.localizedMessage ?: ""
-               )
-           )
-       }
-    }
-
-    override suspend fun getAllFavoriteSongs(): ResponseResult<List<FavoritesDbModel>> {
         return try {
-            val response =favoritesDao.getAllSongs()
-            ResponseResult.Success(response)
-        }catch (e:Exception){
+            favoritesDao.addSongToFavorites(favoritesDbModel)
+            ResponseResult.Success(favoritesDbModel)
+        } catch (e: Exception) {
             ResponseResult.Error.IOException(
                 ResultError(
                     errorMessage = e.localizedMessage ?: ""
@@ -35,11 +32,35 @@ class LocalDataSourceImpl @Inject constructor(
         }
     }
 
+    /**
+     * Retrieves all favorite songs.
+     *
+     * @return A ResponseResult containing a list of FavoritesDbModel.
+     */
+    override suspend fun getAllFavoriteSongs(): ResponseResult<List<FavoritesDbModel>> {
+        return try {
+            val response = favoritesDao.getAllSongs()
+            ResponseResult.Success(response)
+        } catch (e: Exception) {
+            ResponseResult.Error.IOException(
+                ResultError(
+                    errorMessage = e.localizedMessage ?: ""
+                )
+            )
+        }
+    }
+
+    /**
+     * Deletes a song from favorites.
+     *
+     * @param favoritesDbModel The FavoritesDbModel to be deleted.
+     * @return A ResponseResult containing the deleted FavoritesDbModel.
+     */
     override suspend fun deleteSongToFavorites(favoritesDbModel: FavoritesDbModel): ResponseResult<FavoritesDbModel> {
         return try {
             favoritesDao.deleteSongToFavorites(favoritesDbModel)
             ResponseResult.Success(favoritesDbModel)
-        }catch (e:Exception){
+        } catch (e: Exception) {
             ResponseResult.Error.IOException(
                 ResultError(
                     errorMessage = e.localizedMessage ?: ""
@@ -48,11 +69,17 @@ class LocalDataSourceImpl @Inject constructor(
         }
     }
 
+    /**
+     * Retrieves a favorite song with a specific ID.
+     *
+     * @param id The ID of the favorite song to be retrieved.
+     * @return A ResponseResult containing the retrieved FavoritesDbModel.
+     */
     override suspend fun getFavoriteSongWithId(id: Int): ResponseResult<FavoritesDbModel> {
         return try {
-           val response =favoritesDao.getFavoriteSongWithId(id)
+            val response = favoritesDao.getFavoriteSongWithId(id)
             ResponseResult.Success(response)
-        }catch (e:Exception){
+        } catch (e: Exception) {
             ResponseResult.Error.IOException(
                 ResultError(
                     errorMessage = e.localizedMessage ?: ""
